71849fe8a6dab0f84a7acf8bafaf8740230a7d19
Christian Ugwoke <uchennachristian373@gmail.com>
2026-06-28 11:21:30 +0300

    Types of gear box and gear box have been added

---
+++ BMW/GearBox.cs
@@ -0,0 +1,7 @@
+﻿namespace BMW;
+
+public class GearBox
+{
+    public GearBoxType Type { get; set; }
+    public int AmountOfGear { get; set; }
+}
---
+++ BMW/GearBoxType.cs
@@ -0,0 +1,8 @@
+﻿namespace BMW;
+
+public enum GearBoxType
+{
+    Automatic,
+    Manual,
+    SemiAutomatic
+}
