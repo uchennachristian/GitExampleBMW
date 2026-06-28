3c62ec62e692637cec1a993b207d367714446cf1
Christian Ugwoke <uchennachristian373@gmail.com>
2026-06-28 10:38:42 +0300

    Car engine has been added

---
+++ BMW/Engine.cs
@@ -0,0 +1,20 @@
+﻿namespace BMW;
+
+public class Engine
+{
+    public int Weight { get; set; }
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int HorsePower { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine("start");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("turn off");
+    }
+    
+}
