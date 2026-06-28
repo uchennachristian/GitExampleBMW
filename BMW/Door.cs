8bf16ef3f229d8d3bea7ed063aa9d3af0b7a0e31
Sutotsky Vitaly <vitaly@sutotsky.com>
2026-06-28 11:36:37 +0300

    Door has been added

---
+++ BMW/Door.cs
@@ -0,0 +1,19 @@
+﻿namespace BMW;
+
+public sealed class Door
+{
+    
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public void Open()
+    {
+        Console.WriteLine("open");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("close");
+    }
+    
+}
