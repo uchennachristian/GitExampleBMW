e917ccd814998dd9127d4d13ca36add7c3ac3f29
Christian Ugwoke <uchennachristian373@gmail.com>
2026-07-02 18:04:11 +0300

    HeadLamp is corrected

--- BMW/HeadLamp.cs
+++ BMW/HeadLamp.cs
@@ -2,7 +2,7 @@
 
 public class HeadLamp
 {
-    public string HeadlightTypes { get; set; } // TODO: Fix it. You need to replace string to enum.
+    public string HeadlightTypes { get; set; }
     public int Size { get; set; }
     public double Brightness { get; set; }
 
